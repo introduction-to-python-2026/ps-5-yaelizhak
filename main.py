75b9ff36ac40efaeaf7ac665cd61366732cb3d3c
YaelIzhak <yaelizhak2001@gmail.com>
2025-11-26 13:29:53 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-# Add the import statements for functions from string_utils.py and equation_utils.py here
-from string_utils.py import count_atoms_in_reaction
-from equation_utils.py import parse_chemical_reaction, build_equations, my_solve
+import string_utils as su, equation_utils as eu
 
 def balance_reaction(reaction): #"Fe2O3 + H2 -> Fe + H2O"
 
     # 1.parse reaction
-    reactants, products = parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
-    reactant_atoms = count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
-    product_atoms = count_atoms_in_reaction(products)
+    reactants, products = su.parse_chemical_reaction(reaction) # [""Fe2O3", "H2"], ["Fe", "H2O""]
+    reactant_atoms = su.count_atoms_in_reaction(reactants) # [{"Fe":2, "O":1}, {"H":2}]
+    product_atoms = su.count_atoms_in_reaction(products)
 
     # 2.build equation and solve
-    equations, coefficients = build_equations(reactant_atoms, product_atoms)
-    coefficients = my_solve(equations, coefficients) + [1]
+    equations, coefficients = eu.build_equations(reactant_atoms, product_atoms)
+    coefficients = eu.my_solve(equations, coefficients) + [1]
 
     return coefficients # [1/3, 1, 2/3, 1]
+
+
+
 
 
 
